97db59760c225685be6628c9cb963c3d2776edba
Lucas Afonso <56092251+lauf8@users.noreply.github.com>
2023-04-10 13:38:51 -0300

    Update questão3.py

--- questão3.py
+++ questão3.py
@@ -14,36 +14,126 @@ def remover_faturamentos_zero(relatorio):
 relatorio_bruto = {
   "mes": "abril",
   "faturamento": [
-    { "dia": 1, "valor": 1500 },
-    { "dia": 2, "valor": 2500 },
-    { "dia": 3, "valor": 3200 },
-    { "dia": 4, "valor": 1200 },
-    { "dia": 5, "valor": 1800 },
-    { "dia": 6, "valor": 2100 },
-    { "dia": 7, "valor": 3000 },
-    { "dia": 8, "valor": 3500 },
-    { "dia": 9, "valor": 2700 },
-    { "dia": 10, "valor": 4000 },
-    { "dia": 11, "valor": 2900 },
-    { "dia": 12, "valor": 0 },
-    { "dia": 13, "valor": 1800 },
-    { "dia": 14, "valor": 2900 },
-    { "dia": 15, "valor": 3200 },
-    { "dia": 16, "valor": 2700 },
-    { "dia": 17, "valor": 3800 },
-    { "dia": 18, "valor": 4200 },
-    { "dia": 19, "valor": 2800 },
-    { "dia": 20, "valor": 3300 },
-    { "dia": 21, "valor": 2900 },
-    { "dia": 22, "valor": 3100 },
-    { "dia": 23, "valor": 0 },
-    { "dia": 24, "valor": 4100 },
-    { "dia": 25, "valor": 3500 },
-    { "dia": 26, "valor": 4200 },
-    { "dia": 27, "valor": 2800 },
-    { "dia": 28, "valor": 0 },
-    { "dia": 29, "valor": 3300 },
-    { "dia": 30, "valor": 3600 }
+ {
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
   ]
 }
 
